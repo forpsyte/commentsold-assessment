9faa9e980e98c207d28bbc472d1e529d6a86dd46
Justin Simon <jsimon@visiture.com>
2021-10-25 01:33:44 -0400

    Updated link on Dashboard page

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="mb-8 font-bold text-3xl">Dashboard</h1>
-    <p class="mb-8 leading-normal">Hey there! Welcome to your dashboard. Please view the source code for this application <a class="text-indigo-500 underline hover:text-orange-600" href="https://inertiajs.com">here</a>.</p>
+    <p class="mb-8 leading-normal">Hey there! Welcome to your dashboard. Please view the source code for this application <a class="text-indigo-500 underline hover:text-orange-600" href="https://github.com/forpsyte/commentsold-assessment">here</a>.</p>
   </div>
 </template>
 
